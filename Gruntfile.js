c65c98cbf6d87a3187ffbe54489885f633b2b0fd
Simon de Moor <simondemoor0@gmail.com>
2017-03-09 19:02:55 -0800

    khgb

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         'Gruntfile.js',
         'app/**/*.js',
         'public/**/*.js',
-        './*.js',
+        // './*.js',
         'spec/**/*.js'
       ]
     },
